1340d735b6a21765ca18152ade9d244cde3ffbd0
Nguyen Duy Hung <127693983+kharina21@users.noreply.github.com>
2025-04-10 22:05:38 +0700

    initial project

---
+++ itviec-backend/src/test/java/com/example/itviecbackend/ItviecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itviecbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItviecBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
